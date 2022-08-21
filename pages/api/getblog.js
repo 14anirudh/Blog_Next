75ffc5fa989ae182a54de9eb25c432d6d5fbb3b5
anirudh gautam <anirudhgautam987@gmail.com>
2022-08-21 20:18:33 +0530

    added routes

---
+++ pages/api/getblog.js
@@ -0,0 +1,15 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import * as fs from 'fs';
+
+//http://localhost:3000/api/getblog?slug=learn_js
+export default function handler(req, res) {
+  fs.readFile(`blogData/${req.query.slug}.json`,'utf-8',(err,data)=>{
+    if(err){
+        res.status(500).json({error:"No such blog found"})
+    }
+    console.log(req.query.slug)//string to object JSON.parse
+    res.status(200).json(JSON.parse(data))
+  })
+  
+}
+//if slug is valid then it will show data otherwise error would be thrown 
--- pages/blog.js
+++ pages/blog.js
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/Blog.module.css";
 import Link from "next/link";
 
 function blog() {
+  const [display, setDisplay] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setDisplay(data);
+        console.log(data);
+      });
+  }, []);
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <div className="blogItem">
-          <Link href={"BlogPost/Learn.js"}>
-            
-            <h3 className={styles.blogItemh3}>How to learn JavaScript in 2022?</h3>
-          </Link>
-          <p>JavaScript is the language used to design logic for the web</p>
-        </div>
-        <div className="blogItem">
-          <h3 className={styles.blogItemh3}>How to learn JavaScript in 2022?</h3>
-          <p>JavaScript is the language used to design logic for the web</p>
-        </div>
-        <div className="blogItem">
-          <h3 className={styles.blogItemh3}>How to learn JavaScript in 2022?</h3>
-          <p>JavaScript is the language used to design logic for the web</p>
-        </div>
+        {display.map((items) => {
+          return (
+            <div key={items.slug}>
+              <Link href={`BlogPost/${items.slug}`}>
+                <h3 className={styles.blogItemh3}>{items.title}</h3>
+              </Link>
+              <p className={styles.blogItemp}>
+                {items.content.substr(0, 200) + "..."}
+                <Link href={`BlogPost/${items.slug}`}>
+                  <button className={styles.blogbtn}>Read more</button>
+                </Link>
+              </p>
+            </div>
+          );
+        })}
       </main>
     </div>
   );
