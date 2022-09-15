095661d1284c6e94003d6d5365d830b642c0d702
anirudh gautam <anirudhgautam987@gmail.com>
2022-09-15 12:57:36 +0530

    footer added

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 // import '../styles/style.css' would be imoirted on all pages 
@@ -5,7 +6,9 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return<>
   <Navbar/>
-  <Component {...pageProps} /></> 
+  <Component {...pageProps} /> 
+  <Footer/>
+  </>
 }
 
 export default MyApp
